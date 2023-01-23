0bcdfff3cadf03739fbc4163accce81dee357e9b
Jérôme Abel <contact@jeromeabel.net>
2023-01-23 10:16:19 +0100

    Caroussel arrows svg and navigation

--- src/pages/Location/Caroussel/Caroussel.jsx
+++ src/pages/Location/Caroussel/Caroussel.jsx
@@ -1,32 +1,37 @@
-import './Caroussel.scss'
+import { useState } from 'react';
+import styles from './Caroussel.module.scss';
+import { ReactComponent as Arrow } from 'assets/arrow.svg';
 
-// fleche : setIndex ( index +1)
-// fleche : setIndex( index -1)
+export default function Caroussel({ title, pictures }) {
+  // transition ? slide ?
+  const [index, setIndex] = useState(0);
 
-/*
-function nextImage() {
-    if (index === pictures.length - 1) setIndex(0)
-    else setIndex(index++)
-}
+  function nextImage() {
+    if (index === pictures.length - 1) setIndex(0);
+    else setIndex(index + 1);
+  }
 
-function prevImage() {
-     if (index === 0) setIndex(pictures.length - 1)
-    else setIndex(index--)
-}
+  function prevImage() {
+    if (index === 0) setIndex(pictures.length - 1);
+    else setIndex(index - 1);
+  }
 
-*/
+  return (
+    <section className={styles.caroussel}>
+      <div className={styles.caroussel__img}>
+        <img src={pictures[index]} alt={title} />
+      </div>
 
-// img src pictures[index]
-
-export default function Caroussel( { title, pictures }) {
-
-    let index = 0;
-    // transition ? slide ?
-
-    return (
-        <section className='caroussel'>
-            <img src={pictures[index]} alt={title} />
-        </section>
-    )
-
+      {pictures.length > 1 && (
+        <div className={styles.caroussel__controls}>
+          <button onClick={prevImage}>
+            <Arrow />
+          </button>
+          <button onClick={nextImage}>
+            <Arrow />
+          </button>
+        </div>
+      )}
+    </section>
+  );
 }
