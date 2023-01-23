0f0b049c65609f22e7cf6f04dc109a24713c2eb2
Jérôme Abel <contact@jeromeabel.net>
2023-01-23 19:20:54 +0100

    Trying basename to deploy

--- src/App.jsx
+++ src/App.jsx
@@ -6,22 +6,25 @@ import About from './pages/About/About';
 import Location from './pages/Location/Location';
 import Error404 from './pages/Error404/Error404';
 
-const App = createBrowserRouter([
-  {
-    path: '/',
-    element: <Layout />,
-    children: [
-      { index: true, element: <Home /> },
-      { path: 'about', element: <About /> },
-      {
-        path: 'location/:locationId',
-        element: <Location />,
-        errorElement: <Error404 />,
-      },
-      { path: 'error404', element: <Error404 /> },
-      { path: '*', element: <Error404 /> },
-    ],
-  },
-]);
+const App = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <Layout />,
+      children: [
+        { index: true, element: <Home /> },
+        { path: 'about', element: <About /> },
+        {
+          path: 'location/:locationId',
+          element: <Location />,
+          errorElement: <Error404 />,
+        },
+        { path: 'error404', element: <Error404 /> },
+        { path: '*', element: <Error404 /> },
+      ],
+    },
+  ],
+  { basename: '/oc-p11-kasa' }
+);
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { RouterProvider } from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { RouterProvider } from 'react-router-dom';
 
-import App from "./App";
+import App from './App';
 
 import './styles/index.scss';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RouterProvider router={App} />
   </React.StrictMode>
